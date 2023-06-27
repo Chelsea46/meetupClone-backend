53ac4a28f57ba18f02100c7a3bfb98236ca2837b
chelsea roberts <chelsea.j.roberts@hotmail.com>
2023-06-27 06:06:23 +0100

    mongo linked

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://chelseajroberts:Labrinth@meetupcluster.ttrpyxo.mongodb.net/meetupapp?retryWrites=true&w=majority')
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch(error){
         console.log(error)
